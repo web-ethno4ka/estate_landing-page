5c16ad70204bf2a85a2476e4429ce9c80ce68c73
valeriy mixailov <ethno4ka@gmail.com>
2020-09-10 03:48:44 +0300

    Improvements for hamburger menu

--- animation.js
+++ animation.js
@@ -1,9 +1,10 @@
 const navSlide = () => {
-  const burger = document.querySelector('.humb-menu_icon');
+  const burger = document.querySelector('.hamb-menu_icon');
   const nav = document.querySelector('.navigation-items');
 
   burger.addEventListener('click', () => {
-    nav.classList.toggle('.active');
+    nav.classList.toggle('active');
+    burger.classList.toggle('toggle');
   });
 };
 
