5119fef6b445904301882b7dd3de492a7c48572d
valeriy mixailov <ethno4ka@gmail.com>
2020-09-20 23:15:45 +0300

    Adding number animation for Company statistics section

--- number-animation.js
+++ number-animation.js
@@ -1,15 +1,35 @@
-var number = document.querySelector('.number');
-numberTop = number.getBoundingClientRect().top;
-(start = +number.innerHTML), (end = +number.dataset.max);
+// var number = document.querySelector('.number');
+// numberTop = number.getBoundingClientRect().top;
+// (start = +number.innerHTML), (end = +number.dataset.max);
+
+// window.addEventListener('scroll', function onScroll() {
+//   if (window.pageYOffset > numberTop - window.innerHeight / 2) {
+//     this.removeEventListener('scroll', onScroll);
+//     var interval = setInterval(function () {
+//       number.innerHTML = ++start;
+//       if (start == end) {
+//         clearInterval(interval);
+//       }
+//     }, 5);
+//   }
+// });
+
+let number = document.querySelectorAll('.number.quantity');
+let block = document.querySelector('.company-statistics_indicators');
+numberTop = block.getBoundingClientRect().top;
 
 window.addEventListener('scroll', function onScroll() {
   if (window.pageYOffset > numberTop - window.innerHeight / 2) {
     this.removeEventListener('scroll', onScroll);
-    var interval = setInterval(function () {
-      number.innerHTML = ++start;
-      if (start == end) {
-        clearInterval(interval);
-      }
-    }, 5);
+    number.forEach((node) => {
+      let start = parseInt(node.dataset.min);
+      let end = parseInt(node.dataset.max);
+      var interval = setInterval(function () {
+        node.innerHTML = ++start;
+        if (start > end) {
+          clearInterval(interval);
+        }
+      }, 50);
+    });
   }
 });
