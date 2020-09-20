d67ccebc9bc918c72356cc27670707188dfd0f12
valeriy mixailov <ethno4ka@gmail.com>
2020-09-20 23:35:06 +0300

    Final changes

--- number-animation.js
+++ number-animation.js
@@ -1,19 +1,3 @@
-// var number = document.querySelector('.number');
-// numberTop = number.getBoundingClientRect().top;
-// (start = +number.innerHTML), (end = +number.dataset.max);
-
-// window.addEventListener('scroll', function onScroll() {
-//   if (window.pageYOffset > numberTop - window.innerHeight / 2) {
-//     this.removeEventListener('scroll', onScroll);
-//     var interval = setInterval(function () {
-//       number.innerHTML = ++start;
-//       if (start == end) {
-//         clearInterval(interval);
-//       }
-//     }, 5);
-//   }
-// });
-
 let number = document.querySelectorAll('.number.quantity');
 let block = document.querySelector('.company-statistics_indicators');
 numberTop = block.getBoundingClientRect().top;
